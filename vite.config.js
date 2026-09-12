285de43aa306c7b3c3cbccd73308823daba0caa3
Wahyu Kristiawan <whykris97@gmail.com>
2026-09-12 14:58:49 +0700

    refactor: Update Vite watch configuration to ignore specific directories

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
             host: 'app.sollu.test',
         },
         watch: {
-            usePolling: true,
+            ignored: ['**/storage/**', '**/vendor/**', '**/.git/**'],
         },
     },
     plugins: [
